5f99c40a0146cb0b1891608d2ac7e075588f9e6c
sugar <sugr@163.com>
2023-03-27 15:44:07 +0800

    second commit

--- 字典列表.py
+++ 字典列表.py
@@ -1,7 +1,30 @@
-alines =  []
-for aline_number in range(30):
-    new_aline = {'color':'green','point':5,'speed':'slow'}
-    alines.append(new_aline)
-for aline in alines[:5]:
-    print(aline)
-print(len(alines))
+# alines =  []
+# for aline_number in range(30):
+#     new_aline = {'color':'green','point':5,'speed':'slow'}
+#     alines.append(new_aline)
+# for aline in alines[:5]:
+#     print(aline)
+# print(len(alines))
+# alines = []
+# for aline_number in range(30):
+#     new_aline = {'color':'green','point':5,'speed':'slow',}
+#     alines.append(new_aline)
+# for aline in alines[:3]:
+#     if aline['color'] == 'green':
+#         aline['color'] = 'yellow'
+#         aline['speed'] = 'medium'
+#         aline['point'] = 10
+#     elif aline['color'] == 'yellow':
+#         aline['color'] = 'red'
+#         aline['speed'] = 'fast'
+#         aline['point'] = 15
+# for aline in alines[:5]:
+#     print(aline)
+# print("...")
+
+# pizza = {'crust':'thick',
+#          'toppings': ['mushrooms','extra cheese'],
+#          }
+# print(f"You ordered a {pizza['crust']} -crust pizza""with the following toppings:")
+# for topping in pizza['toppings']:
+#     print("\t"+topping)
---
+++ 字典练习.py
@@ -0,0 +1,25 @@
+# 人们
+# people1 = {'first_name':'zhu','last_name':'fengyuan','city':'zhu madian','age':20}
+# people2 = {'firs_name': 'zhao','last_name': 'yixu','city':'zhu madian','age': 20}
+# peoples = []
+# peoples.append(people1)
+# peoples.append(people2)
+# print(peoples)
+# for people in peoples:
+#     print(people)
+#宠物
+# pet1 = {'zhao':'cat'}
+# pet2 = {'zhu':'dog'}
+# pet3 = {'wang':'fish,hi is a water keen'}
+# pets = [pet1,pet2,pet3]
+# print(pets)
+# for pet in pets:
+#     print(pet)
+
+#喜欢的地方
+# place1 = ['zhugong','beijing','tiananmen']
+# place2 = ['guizhou','wuhan','changbaishan']
+# place3 = ['china','henan']
+# favorite_places = {'zhao':place1,'zhu':place2,'wang':place3}
+# for name in favorite_places.keys():
+#     print(f"{favorite_places[name]} is {name} favorite places")
